cc6f7b8cfe41b6e9aaa8b497ae45e2d3032e7d74
amritaa1603 <amritajdhv16033@gmail.com>
2026-03-30 20:40:59 +0530

    Day5

---
+++ Day-6/longest_common_prefix.java
@@ -0,0 +1,34 @@
+
+
+public class longest_common_prefix {
+    
+   
+        public class Solution {
+    
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        
+        String prefix = strs[0];
+        
+        for (int i = 1; i < strs.length; i++) {
+          
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs1 = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs1)); 
+        String[] strs2 = {"dog","racecar","car"};
+        System.out.println(longestCommonPrefix(strs2)); 
+    }
+}
+    
+
+}
